f7badadf1007bc34ffe462a5349210b530ca0c7e
Dmitriy <vysotskij08@bk.ru>
2025-10-29 21:42:06 +0300

    add main logic to ws("add" action)

--- app/core/api/crud/managers.py
+++ app/core/api/crud/managers.py
@@ -1,9 +1,11 @@
 import json
-from typing import List
+from typing import List, Any
 
 from fastapi import APIRouter
 from fastapi import WebSocket
 from starlette.websockets import WebSocketDisconnect
+
+from app.core.api.crud.validation import data_validate
 
 
 class ConnectionManager:
@@ -29,12 +31,7 @@ manager = ConnectionManager()
 
 users: dict[str, List[str]] = {}
 
-all_queues: dict[str, List[str]] = {}
-
-
-@ws_router.get("/hello")
-async def hello():
-    return {"hello": "world"}
+all_queues: dict[str, List[Any]] = {}
 
 
 @ws_router.websocket("/ws/queue")
@@ -43,10 +40,18 @@ async def ws_endpoint(websocket: WebSocket):
     try:
         while True:
             message = await websocket.receive_text()
-            data = json.loads(message)
-            if data["type"] == "message":
-                await websocket.send_text("some data")
-
+            raw = json.loads(message)
+            data = data_validate(raw)
+            if data.type == "add":
+                if data.event not in all_queues:
+                    all_queues[data.event] = []
+                if data.id in all_queues[data.event]:
+                    raise Exception(f"Duplicate event '{data.event}'")
+                all_queues[data.event].append(data.id)
+            print(all_queues)
+            new_data = data.model_dump()
+            new_data["queue"] = all_queues[data.event]
+            await websocket.send_text(json.dumps(new_data))
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         print("disconnect {websocket}", websocket)
